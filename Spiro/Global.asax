f93bfb33f22047ac7ebc59c870ffc3118feef257
scascarini <scascarini@nakedobjects.org>
2013-07-05 15:23:58 +0100

    continue refactoring

--- Spiro/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Spiro.Modern.MvcApplication" Language="C#" %>
--- Spiro/Scripts/spiro.modern.presentations.js
+++
@@ -1,8 +0,0 @@
-var Spiro;
-(function (Spiro) {
-    (function (Modern) {
-        Modern.presentationFactory;
-    })(Spiro.Modern || (Spiro.Modern = {}));
-    var Modern = Spiro.Modern;
-})(Spiro || (Spiro = {}));
-//@ sourceMappingURL=spiro.modern.presentations.js.map
