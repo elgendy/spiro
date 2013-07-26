d9db586c7670306a5cf2f1fdd8a4d6c75b31144e
scascarini <scascarini@nakedobjects.org>
2013-07-26 09:48:01 +0100

    start adding tests for angular

---
+++ Spiro.Angular/Tests/specs/demotest.js
@@ -0,0 +1,6 @@
+describe("a test", function () {
+    it("to be true", function () {
+        expect(true).not.toBe(false);
+    });
+});
+//@ sourceMappingURL=demotest.js.map
---
+++ Spiro.Angular/Tests/specs/demotest.ts
@@ -0,0 +1,8 @@
+/// <reference path="../../Scripts/typings/jasmine/jasmine.d.ts" />
+
+
+describe("a test", function () {
+    it("to be true", function () {
+        expect(true).not.toBe(false);
+    }); 
+}); 
